cf24a69f51f537fd9bfc267e2284b9b6452bd7fb
Michael Gene Brockus <55331536+troglobyte-coder@users.noreply.github.com>
2022-06-13 10:46:19 -0700

    Update and rename prog.py to meson-ui.py

--- meson-ui.py
+++ meson-ui.py
@@ -12,7 +12,7 @@ import sys
 # If we run uninstalled, add the script directory to sys.path to ensure that
 # we always import the correct program modules even if PYTHON_PATH is mangled
 exe = Path(sys.argv[0]).resolve()
-if (exe.parent / 'prog').is_dir():
+if (exe.parent / 'meson-ui').is_dir():
     sys.path.insert(0, str(exe.parent))
 
 
